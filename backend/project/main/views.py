486453d111a70def5f9b442f7f0b8aa0d6ebe049
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-02-16 10:13:04 +0500

    end Django rest app + Create aiogram bot

--- backend/project/main/views.py
+++ backend/project/main/views.py
@@ -1,3 +1,32 @@
+import random
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+from .models import Words
+
+
+class WordsSerializator(serializers.ModelSerializer):
+
+    class Meta:
+        model = Words
+        fields = ["pk","word","gender"]
+
+
+class RandomWord(APIView):
+    def get(self,*args,**kwargs):
+        all_words = Words.objects.all()
+        random_word = random.choice(all_words)
+        serialized_random_word = WordsSerializator(random_word, many=False)
+        return Response(serialized_random_word.data )
+
+class NextWord(APIView):
+    def get(self,request,pk,format=None):
+        word = Words.objects.filter(pk__gt=pk).first()
+        if not word:
+            return HttpResponseNotFound()
+        ser_word = WordsSerializator(word, many=False)
+        return Response(ser_word.data)
--- backend/project/project/urls.py
+++ backend/project/project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("random/", views.RandomWord.as_view()),
+    path("next/<int:pk>", views.NextWord.as_view())
 ]
