0199694fe6c88da6e4b15f4d830fe776e90a7db9
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-02-16 10:36:24 +0500

    add bot app

--- bot/src/bot.py
+++ bot/src/bot.py
@@ -1,3 +1,8 @@
 from local_settings import API_KEY
+from aiogram import Bot,Dispatcher,executor
 
 bot = Bot(token=API_KEY)
+dp = Dispatcher(bot)
+
+if __name__ == '__main__':
+    executor.start_polling(dp,skip_updates=True)
