8930c06c2533222b09139dc2eca91479caa0f6b8
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-02-17 11:35:15 +0500

    bot and django db integration

--- bot/src/bot_app/bot.py
+++ bot/src/bot_app/bot.py
@@ -1,6 +1,8 @@
 from .local_settings import API_KEY
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 
 bot = Bot(token=API_KEY)
-dsp = Dispatcher(bot)
+dsp = Dispatcher(bot, storage=MemoryStorage())
 
--- bot/src/bot_app/random_ten.py
+++ bot/src/bot_app/random_ten.py
@@ -1,6 +1,6 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-from . bot import dsp
+from . bot import dsp,bot
 from . states import GameStates
 from .keyboards import inline_kb
 from .data_fetcher import get_random
@@ -14,4 +14,31 @@ async def random_train(message : types.Message,state : FSMContext):
         data["step"] = 1
         data["answer"] = res.get("gender")
         data["word"] = res.get("word")
+
         await message.reply(f"{data['step']} of 10. This is word = {data['word']}", reply_markup=inline_kb)
+
+@dsp.callback_query_handler(lambda c:c.data in ["das","die","der"])
+async def button_click_callback(callback_query : types.CallbackQuery, state : GameStates.random_ten):
+    await bot.answer_callback_query(callback_query.id)
+    answer = callback_query.data
+    async with state.proxy() as data:
+        if answer.data == data.get("answer"):
+            res = await get_random()
+            data["step"] += 1
+            data["answer"] = res.get("gender")
+            data["word"] = res.get("word")
+            if data["step"] > 10:
+                await bot.send_message(callback_query.from_user.id, "Game over !")
+                await GameStates.start.set()
+            else:
+                await bot.send_message(
+                    callback_query.from_user.id ,
+                    f"YES ! \n {data['step']} of 10. This is word = {data['word']}",
+                    reply_markup=inline_kb
+                    )
+        else:
+            await bot.send_message(
+                callback_query.from_user.id,
+                f"No !",
+                reply_markup=inline_kb
+                )
